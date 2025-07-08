f06f98c9bce8fb774abcb18f979a4602c36a4adf
shafiul <shafiul.alam@tallykhata.com>
2025-07-08 15:06:39 +0600

    modified by shafiul

--- customer_care_portal/display_data/settings.py
+++ customer_care_portal/display_data/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = ''
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['182.163.102.66','supportportal.tallykhata.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['','supportportal.tallykhata.com', 'localhost','127.0.0.1']
 
 # Application definition
 
